555705a7a35c0182a4bb867e5f62eeadc2fb518b
jay-tux <sys.jonas@yahoo.com>
2020-09-28 13:34:24 +0200

    Some actual improvements on JWS

--- JWS/Listener.cs
+++ JWS/Listener.cs
@@ -1,10 +1,56 @@
 using System;
 using System.Net;
+using Jay.Ext;
 
 namespace Jay.Web.Server
 {
     public class Listener
     {
-        
+        private HttpListener Listener { get; private set; }
+        public int Port { get; private set; }
+        public string ListenerState { get; private set; }
+        public Listener()
+        {
+            if(!HttpListener.IsSupported)
+            {
+                Logger.LogError(this, "Failed to start HttpListener: not supported.");
+                Environment.Exit(1);
+            }
+            Listener = new HttpListener();
+            GetPrefixes().ForEach(prefix => Listener.Add(prefix));
+        }
+
+        private void GetState()
+        {
+            try
+            {
+                object s = Program.Settings["JWS.Listener.State"];
+                if(s is string state)
+                {
+                    ListenerState = state;
+                }
+                else
+                {
+                    Logger.LogWarning(this, $"JWS.Listener.State config variable is invalid; expected string. Using Debug configuration.");
+                    ListenerState = "Debug";
+                }
+            }
+            catch(JWSException jwse)
+            {
+                Logger.LogWarning(this, $"JWS.Listener.State config variable doesn't exist. Falling back to Debug.");
+                ListenerState = "Debug";
+            }
+        }
+
+        private void GetPort()
+        {
+            //
+        }
+
+        private string[] GetPrefixes()
+        {
+            GetState();
+            GetPort();
+        }
     }
 }
--- JWS/Program.cs
+++ JWS/Program.cs
@@ -1,13 +1,74 @@
 using System;
-using Jay.Web.Server;
+using Jay.IO.Logging;
+using System.IO;
 
-namespace _run
+namespace Jay.Web.Server
 {
     public static class Program
     {
+        public static Jcf Settings;
         public static void Main(string[] args)
         {
-            
+            StaticLogger.LogMessage("_driver", $"Starting server at {DateTime.Now.ToString()}.");
+
+            StaticLogger.LogMessage("_driver", "Loading Settings...");
+            LoadSettings(new string[] {
+                GetHome() + "/.config/jws/jws.jcf", Data() + "/jws/jws.jcf"
+            });
+            StaticLogger.LogMessage("_driver", "Settings succesfully loaded.");
+
+            StaticLogger.LogMessage("_driver", $"Starting Listener loop...");
+            Listener server = new Listener();
+
+            StaticLogger.LogMessage("_driver", $"Server shut down at {DateTime.Now.ToString()}.");
+        }
+
+        private static void LoadSettings(string[] locations)
+        {
+            string f = "";
+            foreach(string loc in locations)
+            {
+                StaticLogger.LogMessage("_driver", $"Trying config location {loc}...");
+                try
+                {
+                    f = File.ReadAllText(loc);
+                }
+                catch(IOException) {}
+            }
+
+            if(f == "")
+            {
+                StaticLogger.LogError("_driver", $"Failed to find any configuration file.");
+                Environment.Exit(2);
+            }
+
+            Settings = JcfParser.Parse(f);
+        }
+
+        public static string GetHome()
+        {
+            var platform = Environment.OSVersion.Platform;
+            if(platform == PlatformID.Unix || platform == PlatformID.MacOSX)
+            {
+                return Environment.GetEnvironmentVariable("HOME");
+            }
+            else
+            {
+                return Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+            }
+        }
+
+        public static string Data()
+        {
+            var platform = Environment.OSVersion.Platform;
+            if(platform == PlatformID.Unix || platform == PlatformID.MacOSX)
+            {
+                return Environment.GetEnvironmentVariable("/usr/local/etc");
+            }
+            else
+            {
+                return Environment.ExpandEnvironmentVariables("%APPDATA%");
+            }
         }
     }
 }
