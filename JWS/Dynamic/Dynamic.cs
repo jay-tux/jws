c60eec2a5828451bf2e6994bfbd62fe0e590698d
jay-tux <sys.jonas@yahoo.com>
2020-12-12 16:22:45 +0100

    Refactored Dynamic to use new JcfHelper functionality

--- JWS/Dynamic/Dynamic.cs
+++ JWS/Dynamic/Dynamic.cs
@@ -1,6 +1,7 @@
 using System;
 using Jay.Web.Server;
 using Jay.IO.Logging;
+using Jay.Config;
 
 namespace Jay.Web.Server.Dynamic
 {
@@ -9,28 +10,24 @@ namespace Jay.Web.Server.Dynamic
         //private string GetCommand(string ext) {}
         public static void Load()
         {
-            try {
-                object s = Program.Settings["JWS.Dynamic.Enabled"];
-                if(s is string state)
+            JcfResult<string> state = Program.Settings.GetString("JWS.Dynamic.Enabled");
+            if(state)
+            {
+                if((string)state == "off")
                 {
-                    if(state == "off")
-                    {
-                        Program.Logger.LogFormatted("_boot_dyn", "Disabling Dynamic server...", LogSeverity.Message);
-                        return;
-                    }
-                    else if(state != "on")
-                    {
-                        Program.Logger.LogFormatted("_boot_dyn", "Setting JWS.Dynamic.Enabled should be either 'on' or 'off'. Assuming off.", LogSeverity.Warning);
-                        return;
-                    }
+                    Program.Logger.LogFormatted("_boot_dyn", "Disabling Dynamic server...", LogSeverity.Message);
+                    return;
                 }
-                else
+                else if((string)state != "on")
                 {
-                    Program.Logger.LogFormatted("_boot_dyn", "JWS.Dynamic.Enabled should be a string. Assuming 'off'.", LogSeverity.Warning);
+                    Program.Logger.LogFormatted("_boot_dyn", "Setting JWS.Dynamic.Enabled should be either 'on' or 'off'. Assuming off.", LogSeverity.Warning);
                     return;
                 }
             }
-            catch(Exception) {}
+            else
+            {
+                Program.Logger.LogFormatted("_boot_dyn", "JWS.Dynamic.Enabled is not (correctly) set. Assuming off.", LogSeverity.Warning);
+            }
         }
     }
 }
